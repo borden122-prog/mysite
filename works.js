3e45088cc8237b4ca05ad028bb3b2e790df64f61
borden122 <borden122@gmail.com>
2026-04-22 23:16:16 +0300

    Add 11.webm

--- works.js
+++ works.js
@@ -1,5 +1,6 @@
 const WORKS = [
 
+  { type: 'video', src: 'assets/11.webm' },
   { type: 'video', src: 'assets/10.webm' },
   { type: 'video', src: 'assets/9.webm' },
   { type: 'video', src: 'assets/7.webm' },
